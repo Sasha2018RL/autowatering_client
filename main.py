955023a52d1fa8f5204ab1eee9dfad84fec91db0
sasha2018rl <sashakharlamov2004@gmail.com>
2021-07-18 15:26:07 +0300

    Добавлены информационные сообщения

--- main.py
+++ main.py
@@ -49,27 +49,36 @@ def restart():
 
 @sio.on('en_watering')
 def en_watering(watering_time):
+    sio.emit('no-port', 'Включаю полив')
     click(10, 3, int(watering_time), 60, True)
+    sio.emit('no-port', 'Включен полив на {} минут'.format(watering_time))
 
 
 @sio.on('ds_watering')
 def ds_watering():
+    sio.emit('no-port', 'Отключаю полив')
     click(4)
+    sio.emit('no-port', 'Полив отключен')
 
 
 @sio.on('hard_reset')
 def hard_reset():
+    sio.emit('no-port', 'Сбрасываю')
     reset()
 
 
 @sio.on('en_usage')
 def en_usage(usage_time):
+    sio.emit('no-port', 'Включаю использование')
     click(5, 4, int(usage_time), 600, True)
+    sio.emit('no-port', 'Включено использование на {} минут'.format(usage_time))
 
 
 @sio.on('ds_usage')
 def ds_usage():
+    sio.emit('no-port', 'Отключаю использование')
     click(3)
+    sio.emit('no-port', 'Использование отключено')
 
 
 def click(count, reset_count=0, timeout=0, max_time=60, reset_at_end=False):
@@ -91,7 +100,7 @@ def click(count, reset_count=0, timeout=0, max_time=60, reset_at_end=False):
 
 def reset():
     os.system('bash /root/reset.sh')
-    sio.emit('no-port', 'reset performed')
+    sio.emit('no-port', 'Arduino перезапущена!')
 
 
 sio.connect('wss://sasha.hillel.it:8443/')
